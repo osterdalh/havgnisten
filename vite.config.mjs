5583288e1e6bf45a9c87c777fa4b6cfb5daa0035
m01534 <hans.osterdal@skatteetaten.no>
2026-02-23 21:44:02 +0100

    skifte til vite

---
+++ vite.config.mjs
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.mjs',
+  },
+});
